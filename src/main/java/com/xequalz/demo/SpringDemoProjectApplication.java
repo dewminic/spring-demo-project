4bb46b1fe1fce370a8fb840cdabac08e696a2bf2
Dewmini <Dewmini@DESKTOP-GF01IKQ>
2022-07-04 15:56:01 +0530

    Initial Commit

---
+++ src/main/java/com/xequalz/demo/SpringDemoProjectApplication.java
@@ -0,0 +1,15 @@
+package com.xequalz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.xequalz.demo.util.MyConstant;
+
+@SpringBootApplication
+public class SpringDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/xequalz/demo/util/MyConstant.java
@@ -0,0 +1,6 @@
+package com.xequalz.demo.util;
+
+public class MyConstant {
+
+	public static final String SAMPLE_VALUE = "example";
+}
